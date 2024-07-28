2fa1a2a4e28124c6d9a7e1a08e93b1e1eeef9569
Lazar Todorovic <vulalaza@gmail.com>
2024-07-28 18:30:44 +0200

    added express api

---
+++ apps/chat-client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  title = 'chat-client';
+  http = inject(HttpClient)
+  ngOnInit(): void {
+      this.http.get('http://localhost:3333/api').subscribe({ next:console.log, error: console.error })
+  }
+}
